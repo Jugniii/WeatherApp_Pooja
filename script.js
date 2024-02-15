72ebae555b89dc1e700e407396d54a9b61bbe872
Pooja <batuleps21@gmail.com>
2024-02-15 12:17:02 +0530

    I have done some changes related to the background.

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ async function fetchWeatherCity(cityName){
 	let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=2acbac7c6cedabb2fc01a529b86ede80`;
 	const response= await fetch(url)
 	const data= await response.json();
+	let city= document.getElementById("city");
+	city.innerHTML=data.name;
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML=data.wind.speed + "km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+"g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.temp+"°C";
 	console.log(data)
 }
 //demo()
 
 function fetchCity(){
-	let cityName = document.getElementById("city_name");
+	let cityName = document.getElementById("cityname");
 	if(cityName.value===""){
 		alert("Enter a city name");
 	}else{
